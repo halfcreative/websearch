ebff7d426aa5a7fdcb72c1b40a10b976f4b1e851
Alan Sato <halfcreative@gmail.com>
2018-04-07 16:54:09 -0400

    first commit

---
+++ html2.py
@@ -0,0 +1,176 @@
+#! /usr/bin/python3
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
+import re
+
+
+# Sam's First WebCrawler... IMPROVED to version 2
+## INTRO:
+## Welcome to the crawler! I made this for my AI class
+## The crawler has 1 function that takes a URL to crawl
+## It will make 1 text file for each page visited.
+## In each text file, the top will have the URL of the page visited, the number of relative links on the site, and the number of absolute links on the site
+##      In addition, the HTML contents will be included.
+## For Extra Credit:
+##      Phone Number finding
+
+
+#CHANGELOG
+#--------- 
+#4/7/2018-2.2
+#2.2:
+#-Started Extra Credit:
+#       -Added Phone number regex
+#---------
+#4/6/2018-2.1.1
+#2.1.1:
+#-Added Limiter
+#---------
+#4/3/2018-Update to v2.1
+#2.1:
+#-Stopped visiting unneccesary stuff(pdf's ppts etc)
+#-Some cleaning up of the code
+#-Added tasks
+#---------
+#4/1/2018-Created Version2
+#2.0:
+#-Switch from recursive to iterative. Recursive returned error
+#---------
+
+#To-Do:
+#--Added other Regular Expressions to find additonal information, in order to gain extra credit
+#################################################
+
+
+#######################################
+#             Debugging               #
+#######################################
+DEBUG = True
+
+def log(n):
+    if DEBUG == True:
+        print(n)
+#######################################
+
+class crawler():
+    def __init__(self):
+        self.baseurl = ""
+        self.visited = [] # ALL VISITED LINKS INCLUDING NON-HTML Links
+        self.tovisit = [] # LINKS on the list to visit
+        self.filesmade = 0
+        self.linksfound = 0
+        self.rellinksfound = 0
+        self.abslinksfound = 0
+        self.phones = []
+        return
+
+    #Crawl will have the crawler find every link on the page, store the ones not visited in a list, then visit each page on the domain
+    def crawl(self,site,limit=0):
+        if limit==0:
+            limited = False
+            print("Crawling Full site")
+        else:
+            limited = True
+            print("Crawing {0} pages".format(limit))
+        
+        #Sets a baseurl that can be compared to and adds the first URL on the list of places to visit
+        self.baseurl = site
+        self.tovisit.append(site)
+
+        #Regular Expressions to parse page. Only want/need to make these objects once.
+        #regex - a regular expression to parse the base URL for the name and extension ie muhlenberg and .edu
+        regex = re.compile(r'https?:\/\/(www\.)?(.+)\.(.+)$')
+        baseurlbegin = regex.match(self.baseurl).group(2) # for baseurl https://www.muhlenberg.edu this returns "muhlenberg"
+        baseurlend = regex.match(self.baseurl).group(3)# for baseurl https://www.muhlenberg.edu this returns "edu"
+        #regex1 - All links, as marked by href
+        regex1 = re.compile(r'href=[\'"]?([^\'" >]+)')
+        #regex2 - All relative links
+        regex2 = re.compile(r'^\/.+[^pdf|css|pptx?|docx?|png|jpeg|gif]$')
+        #regex3 - All absolute links
+        regex3 = re.compile(r'https?:\/\/(www\.)?'+baseurlbegin+'\.'+baseurlend+'(.+[^pdf|css|pptx?|docx?|png|jpeg|gif])$')
+        #regex4 - Phone Numbers
+        regex4 = re.compile(r'\([0-9]{3}\)|[0-9]{3}[-,.\s]?[0-9]{3}[-,.\s]?[0-9]{4}')
+        #regex5 - 
+        regex5 = re.compile(r'.+@[.+]\.[com|org|edu]')
+
+        #Heres where the actual crawiling starts!
+        while len(self.tovisit) > 0:
+            visiting = self.tovisit.pop(0)
+            log("Going to {0}".format(visiting))
+            #make a request object with the 'visiting' link and a header.
+            #the header is not super necessary, but I was getting 403 errors and this was a suggested solution
+            req = Request(visiting,headers={'User-Agent': 'Mozilla/5.0'}) 
+            try:
+                #Try to make a page request
+                pagereq = urlopen(req)
+            except HTTPError as e:
+                log("Page {0} returned error {1}".format(visiting,e))
+                self.visited.append(visiting)
+            else:
+                #Page Request successful! read the page request as bytes
+                bytepage = pagereq.read()
+                try:
+                    #Try to decode the bytepage in utf-8, this will only work if its a css or html page
+                    page = bytepage.decode('utf-8')
+                except UnicodeDecodeError as err:
+                    log("link {0} returned {1} : probably because its not a page but a pdf or something".format(visiting,err))
+                else:
+                    #THE PAGE WAS DECODED!
+                    
+
+                    #Find Phone numbers
+                    phone_nums = regex4.findall(page)
+                    for number in phone_nums:
+                        if not any(number in records for records in self.phones):
+                            self.phones.append((number,visiting))
+                    #Find Links:
+                    #create initial list of links and make a filter list of all the links with slash
+                    links = regex1.findall(page)
+                    linksrel = list(filter(regex2.match,links))
+                    linksabs = [m.group(2) for m in (regex3.match(a) for a in links) if m] #creates a list of the absolute links for this domain
+
+                    #Make Page and Add to visit:
+                    self.makepage(self.filesmade,visiting,len(linksrel),len(linksabs),page)
+                    self.visited.append(visiting)
+
+                    #End the program if the number of files made would exceed the limit
+                    if limited and self.filesmade >= limit:
+                        print("Reached specified limit:{0}".format(limit))
+                        break
+                    #Continue to search for links
+                    for i in range(len(linksrel)):     
+                        check = self.baseurl + linksrel[i] #relative links + base url for an absolute link
+                        if check not in self.visited and check not in self.tovisit :
+                            self.tovisit.append(check)
+                            #log("adding {0} to visit".format(check))
+                            self.linksfound += 1
+                            self.rellinksfound += 1
+                    #Add absolute links to tovisit if its not been visited or its not already on the list 
+                    for j in range(len(linksabs)):     
+                        check = self.baseurl + linksabs[j] #relative links + base url for an absolute link
+                        if check not in self.visited and check not in self.tovisit :
+                            self.tovisit.append(check)
+                            log("adding {0} to visit".format(check))
+                            self.linksfound +=1
+                            self.abslinksfound += 1
+
+        print("Finished Crawling\nCrawler found and recorded {0} pages on {1}".format(self.filesmade,site))
+        print("There were {0} unique links, {1} of them relative and {2} of them absolute".format(self.linksfound,self.rellinksfound,self.abslinksfound))
+        print("Found {0} phone numbers".format(len(self.phones)))
+        print(self.phones)
+        print("Found {0} emails".format(len(self.emailadds)))
+        print(self.emailadds)
+
+    #Makes page with the order visited, the number of links relative and absolute and the content of the html
+    def makepage(self,num,site,relative,absolute,content):
+        title = "page#" + str(num) + ".txt"
+        page = open(title,'w+')
+        self.filesmade +=1
+        page.write("URL:{0}\nNumber of Relative Links:{1}\nNumber of Absolute Links:{2}\n".format(site,relative,absolute))
+        page.write(content)
+        page.close()
+
+def main():
+    webby = crawler()
+    webby.crawl("http://www.muhlenberg.edu",10)
+main()
