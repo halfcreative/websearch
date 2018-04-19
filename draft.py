2a0b5262b9611d85419d350786a2282c1b6e6d3c
squate <tobogganlad@gmail.com>
2018-04-19 13:31:06 -0400

    made file

---
+++ draft.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 19 13:28:25 2018
+
+@author: nate
+"""
+
+def main():
+    # load GUI, w input for domain and search term
+    # if domain is not on file, ask user if they'd like to make a file.
+    # if they would, crawl us a new mf database
+    # ef
+    return 5
