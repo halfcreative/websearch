fabd1961680f18ce0e4b497036b359f1f19b54dc
squate <tobogganlad@gmail.com>
2018-04-19 13:45:58 -0400

    added a thing

--- draft.py
+++ draft.py
@@ -10,5 +10,18 @@ def main():
     # load GUI, w input for domain and search term
     # if domain is not on file, ask user if they'd like to make a file.
     # if they would, crawl us a new mf database
-    # ef
-    return 5
+    
+    domain = input("What domain are we searchin?: ")
+    
+    try:
+        open('{0}/{0}#0'.format(domain), 'r')
+    except FileNotFoundError as e:
+        print('file does not exist')
+        #make directory for this domain
+    else:
+        print("file exists")
+    
+    
+    
+    
+main()
