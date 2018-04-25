ab4a5dbdaf008295d9c1d8541422298296dc2bed
squate <tobogganlad@gmail.com>
2018-04-24 22:24:18 -0400

    fix't

--- revind.py
+++ revind.py
@@ -1,16 +1,16 @@
 class r_index:
         def __init__():
-                self.d = {}
+               d = {}
         def scan_doc(self, doc):
                 text = open(doc, 'r').read()
                 words = preprocess(text)
                 for w in words:
-                        if word not in d.keys:
-                                d[word] = {}
-                        if doc not in d[word].keys:
-                                d[word][doc] = 1
+                        if w not in self.d.keys:
+                                self.d[w] = {}
+                        if doc not in self.d[w].keys:
+                                self.d[w][doc] = 1
                         else:
-                                d[word][doc] += 1
+                                self.d[w][doc] += 1
 
 
 
