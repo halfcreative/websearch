b6f547ba7ecf514258813968c5d51d59e8545762
squate <tobogganlad@gmail.com>
2018-04-25 12:02:10 -0400

    integrating other files

--- draft.py
+++ draft.py
@@ -5,6 +5,8 @@ Created on Thu Apr 19 13:28:25 2018
 
 @author: nate
 """
+from revind import r_index
+import crawler_lite
 
 def main():
     # load GUI, w input for domain and search term
@@ -14,12 +16,19 @@ def main():
     domain = input("What domain are we searchin?: ")
     
     try:
-        open('{0}/{0}#0'.format(domain), 'r')
+        open('{0}/{0}page#0.txt'.format(domain), 'r')
     except FileNotFoundError as e:
-        print('file does not exist')
-        #make directory for this domain
+        decide = input('data does not exist, make new folder? (y/n)')
+        if decide == 'y':#make directory for this domain
+            domaincrawl = crawler_lite.crawler()
+            domaincrawl.crawl(domain)
+        else:
+            print('no problem, ending program!')
     else:
-        print("file exists")
+        print('creating reverse index...')
+        ri = r_index(domain)
+        print(ri.toString())
+        
     
     
     
