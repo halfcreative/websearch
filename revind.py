03d2576b168caeb984f1debe21475a290f0ae70b
squate <tobogganlad@gmail.com>
2018-04-24 23:13:11 -0400

    got it workin with the directory structure

--- revind.py
+++ revind.py
@@ -1,21 +1,39 @@
+import os
+
 class r_index:
-        def __init__():
-               d = {}
+        def __init__(self, dom):
+               self.d = {}
+               self.domain = dom
+
+        def construct(self):
+            if not os.path.exists(self.domain):
+                print('domain folder not found')
+            else:
+                for file in os.listdir(self.domain):
+                    self.scan_doc(file)
+                    
         def scan_doc(self, doc):
-                text = open(doc, 'r').read()
-                words = preprocess(text)
+                text = open(self.domain+'/'+doc, 'r').read()
+                words = text.split()
                 for w in words:
-                        if w not in self.d.keys:
+                        if w not in self.d.keys():
                                 self.d[w] = {}
-                        if doc not in self.d[w].keys:
+                        if doc not in self.d[w].keys():
                                 self.d[w][doc] = 1
                         else:
                                 self.d[w][doc] += 1
-
-
-
-
-
-
-
-
+                                
+        def toString(self):
+            out = 'WORDS:\n'
+            for word in self.d.keys():
+                out += '{0}\n'.format(word)
+                for doc in self.d[word]:
+                    out += '\tdoc: {0} count: {1}\n'.format(doc, self.d[word][doc])
+            return out
+        
+def main():
+    ri = r_index('muhlenberg')
+    ri.construct()
+    strsss = ri.toString()
+    print(strsss)
+main()       
