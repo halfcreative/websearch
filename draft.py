d7d6c0f29b729074566d5c6c461934944fdf544b
squate <tobogganlad@gmail.com>
2018-05-01 14:44:20 -0400

    expanding UX / file-access structure

--- draft.py
+++ draft.py
@@ -6,7 +6,8 @@ Created on Thu Apr 19 13:28:25 2018
 @author: nate
 """
 from revind import r_index
-import crawler_lite
+from crawler_lite import crawler
+import pickle
 
 def main():
     # load GUI, w input for domain and search term
@@ -14,23 +15,37 @@ def main():
     # if they would, crawl us a new mf database
     
     domain = input("What domain are we searchin?: ")
-    
     try:
-        open('{0}/{0}page#0.txt'.format(domain), 'r')
-    except FileNotFoundError as e:
-        decide = input('data does not exist, make new folder? (y/n)')
-        if decide == 'y':#make directory for this domain
-            domaincrawl = crawler_lite.crawler()
-            domaincrawl.crawl(domain)
-        else:
-            print('no problem, ending program!')
+        open('dicts/{0}.pkl'.format(domain), 'r')
+    except FileNotFoundError as e0:
+        choice0 = input('reverse index file does not exist, make file? (y/n)')
+        if choice0 == 'y':
+            
+            try:
+                open('{0}/{0}page#0.txt'.format(domain), 'r')
+            except FileNotFoundError as e:
+                decide = input('data does not exist, make new folder? (y/n)')
+                if decide == 'y':#make directory for this domain
+                    domaincrawl = crawler()
+                    domaincrawl.crawl(domain)
+                else:
+                    print('no problem, ending program!')
+            else:
+                print('creating reverse index...')
+                ri = r_index(domain)
+                ri.make_file()
+                print(ri.toString())
+                if 'javascript' in ri.d.keys():
+                    print('its not filtering out javascript yet')
+                    
+        else: 
+            print('no worries, come back another time.')
     else:
-        print('creating reverse index...')
-        ri = r_index(domain)
-        print(ri.toString())
+        with open('dicts/' + domain + '.pkl', 'rb') as f:
+            dom_ind = pickle.load(f)
+        print('index loaded')
+
+    query = input('what we searchin?')
+    
         
-    
-    
-    
-    
 main()
