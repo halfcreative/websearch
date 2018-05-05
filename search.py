be7f831ed36c0048d82e390be725b7e82ce4566a
squate <35817142+squate@users.noreply.github.com>
2018-05-04 23:45:56 -0400

    added comments

--- search.py
+++ search.py
@@ -5,6 +5,11 @@ Created on Thu Apr 19 13:28:25 2018
 
 @author: nate
 """
+#Nate Levy, Alan Sato
+#search.py: inputs: a preprocessed set of query terms, (q_words), a reverse-index output file(ri)
+#generates document and query vectors and returns urls sorted by cosine similarity 
+#of their document vector to the query vector (descending)
+
 import collections
 import math
 import numpy as np
@@ -13,18 +18,21 @@ import operator
 
 def retrieve(q_words, ri):
     q_vec = []
-    relevant_docs = []  #harvest all docs that contain any q_words
+    #harvest all docs that contain any q_words
+    relevant_docs = []  
     for qw in q_words:
-        if ri.get(qw) != None:    
+        if dict(ri.get(qw)) != None:    #avoid keyerrors at all cost
             for d in ri[qw]:
                 if ri[qw] != 0.0 and d != '<df>' and d != '<total>':
                     relevant_docs.append(d)
-            
-    doclist = list(set(relevant_docs))
+    #remove duplicates       
+    doclist = list(set(relevant_docs)) 
+    
+    #generate document vectors
     docveclist = []
-    for rd in doclist:    #create document vectors
+    for rd in doclist:    
         vec = []
-        for i in range(len(q_words)):
+        for i in range(len(q_words)): #still no keyerrors here, thank you very much
             if ri.get(q_words[i]) == None:
                 weight = 0.0
             elif ri[q_words[i]].get(rd) == None:
@@ -34,9 +42,11 @@ def retrieve(q_words, ri):
             vec.append(weight) # sets the vector to the tf-idf weights as stored in the revese index
         docveclist.append(vec)
     
+    #generate frequency info for later weighting
     freq_info = collections.Counter(q_words)
     max_freq = freq_info.most_common(1)[0][1] #make query vectors
     
+    #do all that math from the powerpoint, trust us it's there
     for i in range(len(q_words)): 
         freq_wq = freq_info[q_words[i]]
         if ri.get(q_words[i]) != None:
@@ -53,13 +63,14 @@ def retrieve(q_words, ri):
         cos_similarity.append(cos_sim(q_vec, docveclist[h]))
      
     urllist = []
+    #access the urls corresponding to the scanned documents
     for d in range(len(doclist)):
         urllist.append(ri['<urldict>'][doclist[d]])
     results_w_score = list(zip(urllist, cos_similarity))
     results_w_score.sort(key = operator.itemgetter(1), reverse = True)
     for r in range(len(results_w_score)):
          print('{0}'.format(results_w_score[r]))
-    return results_w_score 
+ 
 def cos_sim(a,b):
     dp = np.dot(a,b)
     aM = np.linalg.norm(a)
@@ -72,5 +83,3 @@ def cos_sim(a,b):
     if cs > 1.0:   
         cs = 1.0
     return cs
-        
-            
